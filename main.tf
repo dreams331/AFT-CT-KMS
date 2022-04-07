dd7aee6b0b9817266b77af0805021a74877a8db9
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-04-07 10:24:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,3 +52,107 @@ resource "aws_kms_alias" "replica" {
   name          = "alias/${var.alias}"
   target_key_id = aws_kms_replica_key.replica.key_id
 }
+#######################################################
+
+
+# FINAL CODE THAT WORKED BELOW
+
+locals {
+  admin_username = var.username
+  account_id     = data.aws_caller_identity.current.account_id
+}
+provider "aws" {
+  region  = var.aws_region
+}
+provider "aws" {
+  alias  = "replica"
+  region = var.replica_region
+}
+provider "aws" {
+  alias  = "replica2"
+  region = var.replica_region2
+}
+resource "aws_kms_key" "kms-primary" {
+  multi_region        = true
+  description         = var.description
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.kms-test.json
+  tags = merge(
+    var.tags,
+    {
+      "Multi-Region" = "true",
+      "Primary"      = "true"
+    }
+  )
+}
+resource "aws_kms_alias" "alias" {
+  name          = "alias/${var.aws_region}-kms-key"
+  target_key_id = aws_kms_key.kms-primary.key_id
+}
+resource "aws_kms_replica_key" "replica1" {
+  provider = aws.replica
+  description             = "kms key description"
+  primary_key_arn         = aws_kms_key.kms-primary.arn
+  policy                  = data.aws_iam_policy_document.kms-test.json
+  tags = merge(
+    var.tags,
+    {
+      "Multi-Region" = "true",
+      "Primary"      = "false"
+    }
+  )
+}
+# Add an alias to the replica key
+resource "aws_kms_alias" "replica1" {
+  provider = aws.replica
+  name          = "alias/${var.replica_region}-kms-key"
+  target_key_id = aws_kms_replica_key.replica1.key_id
+}
+resource "aws_kms_replica_key" "replica2" {
+  provider = aws.replica2
+  description             = "kms key description"
+  primary_key_arn         = aws_kms_key.kms-primary.arn
+  policy                  = data.aws_iam_policy_document.kms-test.json
+  tags = merge(
+    var.tags,
+    {
+      "Multi-Region" = "true",
+      "Primary"      = "false"
+    }
+  )
+}
+# Add an alias to the replica key
+resource "aws_kms_alias" "replica2" {
+  provider = aws.replica2
+  name          = "alias/${var.replica_region2}-kms-key"
+  target_key_id = aws_kms_replica_key.replica2.key_id
+}
+data "aws_caller_identity" "current" {}
+data "aws_iam_policy_document" "kms-test" {
+  statement {
+    sid       = "Enable IAM User Permissions"
+    effect    = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.account_id}:root"]
+    }
+    actions    = ["kms:*"]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "Allow use of the key"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = ["*"]
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::${local.account_id}:root"]
+    }
+  }
+}
